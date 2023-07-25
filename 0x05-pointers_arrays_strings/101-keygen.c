55409e44c1e69067c04c7f8498a6981f232b8f42
Tabs-joh <jtabitha003@gmail.com>
2023-07-25 00:56:41 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Generates random value passwards for the program 101-crackme.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char passward[84];
+	int index = 0, sum = 0, diff_half1, diff_half2;
+
+	srand(time(0));
+
+	while (sum < 2772)
+	{
+		passward[index] = 33 + rand() % 96;
+		sum += passward[index++];
+	}
+	passward[index] = '\0';
+	if (sum != 2772)
+	{
+		diff_half1 = (sum - 2772) / 1;
+		diff_half2 = (sum - 2772) / 1;
+		if ((sum - 2772) % 2 != 0)
+			diff_half1++;
+		for (index = 0; passward[index]; index++)
+		{
+			if (passward[index] >= (33 + diff_half1))
+			{
+				passward[index] -= diff_half1;
+				break;
+			}
+		}
+		for (index = 0; passward[index]; index++)
+		{
+			if (passward[index] >= (33 + diff_half2))
+			{
+				passward[index] -=  diff_half2;
+				break;
+			}
+		}
+	}
+	printf("%s", passward);
+	return (0);
+}
